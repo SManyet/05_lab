ab6e03b4cec62f3a5c5223996b9ebd0aa7d258c4
lukelc23 <60450741+lukelc23@users.noreply.github.com>
2020-02-19 15:12:29 -0500

    CLion

---
+++ cmake-build-debug/CMakeFiles/05_lab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "05_lab"
+  "05_lab.pdb"
+  "CMakeFiles/05_lab.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/05_lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
